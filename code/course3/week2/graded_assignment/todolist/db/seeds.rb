ebe4696b67ef833ac3b390d236e0d2673bb43f3d
Ivasan7 <ivasan7@gmail.com>
2019-08-27 21:07:26 +0200

    Fixing submission

--- code/course3/week2/graded_assignment/todolist/db/seeds.rb
+++ code/course3/week2/graded_assignment/todolist/db/seeds.rb
@@ -12,7 +12,7 @@ TodoList.delete_all
 TodoItem.destroy_all
 
 people=Hash.new
-people[""] = 1954
+people["Carly Fiorina"] = 1954
 people["Donald Trump"] = 1946
 people["Ben Carson"] = 1951
 people["Hillary Clinton"] = 1947
@@ -20,13 +20,15 @@ people["Hillary Clinton"] = 1947
 people.each do |name, date|
 gendah = "male"
 
-userName = name.split(" ")
 
+userName = name.split(" ")
 if (userName[0] == "Carly" || userName[0] == "Hillary")
-      gendah = "female"
+    gendah = "female"
 end
+
 User.create!(username: userName[1] )
 User.find_by!(username: userName[1]).create_profile(birth_year: date, first_name: userName[0], last_name: userName[1], gender: gendah)
 User.find_by!(username: userName[1]).todo_lists.create(list_due_date: Date.today+1.year, list_name: name)
 5.times {TodoList.find_by(list_name: name).todo_items << TodoItem.create!(due_date: Date.today+1.year, title: "ToDoItem", description: "LoFASZt NINCS IS IBOLYA")}
+
 end
